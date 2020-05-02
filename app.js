d16e3e74bac137da335b177bcbae6514b80efd1b
Richard Zhang <richard_zhang12@yahoo.com>
2020-05-02 06:21:07 -0500

    3rd commit

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// mongoose.connect('mongodb://localhost:27017/userDB', {
-mongoose.connect(process.env.DB_MONGO, {
+mongoose.connect('mongodb+srv://onlinedatingapp:Wanglin12@onlinedatingapp-otcyj.mongodb.net/test?retryWrites=true&w=majority', {
+// mongoose.connect(process.env.DB_MONGO, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false,
